86f3c7c327355a06cc9333d0532e1bbf74229f96
Narendra dubey <176411268+narendra-create@users.noreply.github.com>
2026-06-08 17:25:35 +0530

    UPDATE: Hero section for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      tracklancer
+      <section className="px-4 my-8">
+        <div className="hero-tag text-accent-dim font-sans text-md lg:text-lg">PROJECT MILESTONE TRACKING</div>
+        <div className="flex flex-col font-serif text-2xl gap-0 justify-center font-bold">
+          <h1>Track Work.</h1>
+          <h1 className="text-accent">
+            <i>Get paid.</i>
+          </h1>
+          <h1>On record.</h1>
+        </div>
+        <div>desc</div>
+        <div>buttons</div>
+      </section>
     </main>
   );
 }
