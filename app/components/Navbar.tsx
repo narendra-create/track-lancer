44daeb14d31ec89a835bc25cf058901e83a3941b
Narendra dubey <176411268+narendra-create@users.noreply.github.com>
2026-06-08 14:39:16 +0530

    Created navbar file

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            saa
+        </nav>
+    )
+}
+
+export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Tracklancer</div>
+    <main>
+      tracklancer
+    </main>
   );
 }
