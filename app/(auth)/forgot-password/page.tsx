06add37271d91030c9d694f878f904b986b86a64
Narendra dubey <176411268+narendra-create@users.noreply.github.com>
2026-06-15 23:37:19 +0530

    UI: Fixing ui of forgot password

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -2,7 +2,7 @@ import { ForgotPasswordForm } from "@/app/components/ForgotPasswordForm";
 
 const ForgotPassword = () => {
   return (
-    <main className="h-screen mx-6 lg:mx-auto my-12">
+    <main className="h-screen w-full mx-6 lg:mx-auto my-12">
       <ForgotPasswordForm />
     </main>
   );
