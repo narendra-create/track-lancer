880745160b9de4131c466b7762b24054e0640670
Narendra dubey <176411268+narendra-create@users.noreply.github.com>
2026-06-15 14:26:43 +0530

    UPDATE: Making buttons Links to make them functional

--- app/components/Buttons/Secondbutton.tsx
+++ app/components/Buttons/Secondbutton.tsx
@@ -1,9 +1,18 @@
-import type { Buttonprop } from "./Yellowbutton";
+import Link from "next/link";
+import { ReactNode } from "react";
 
-export function Secondbutton({ children }: Buttonprop) {
+type ButtonProps = {
+  children: ReactNode;
+  href: string;
+};
+
+export function Secondbutton({ children, href }: ButtonProps) {
   return (
-    <button className="rounded-md transition-all ease-in-out duration-150 w-full lg:w-36 text-[10px] text-ink-muted lg:px-4 py-2.5 border hover:text-ink focus:text-ink font-mono border-ink-muted">
+    <Link
+      href={href}
+      className="rounded-md transition-all ease-in-out duration-150 w-full lg:w-36 text-[10px] text-ink-muted lg:px-4 py-2.5 border hover:text-ink focus:text-ink font-mono border-ink-muted text-center"
+    >
       {children}
-    </button>
+    </Link>
   );
 }
--- app/components/Buttons/Yellowbutton.tsx
+++ app/components/Buttons/Yellowbutton.tsx
@@ -1,9 +1,18 @@
-export type Buttonprop = {
-  children: string;
+import Link from "next/link";
+import { ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+  href: string;
 };
 
-export function YellowButton({ children }: Buttonprop) {
+export function YellowButton({ children, href }: ButtonProps) {
   return (
-    <button className="rounded-md transition-all ease-in-out duration-150 w-full lg:w-36 text-[10px] text-accent lg:px-4 py-2.5 border hover:bg-accent hover:text-black focus:bg-accent focus:text-black font-mono border-accent">{children}</button>
+    <Link
+      href={href}
+      className="rounded-md transition-all ease-in-out duration-150 w-full lg:w-36 text-[10px] text-accent lg:px-4 py-2.5 border hover:bg-accent hover:text-black focus:bg-accent focus:text-black font-mono border-accent text-center"
+    >
+      {children}
+    </Link>
   );
 }
