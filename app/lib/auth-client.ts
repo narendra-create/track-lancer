b81f692704bc253e12b4ab1e22ce6426489a520c
Narendra dubey <176411268+narendra-create@users.noreply.github.com>
2026-06-15 23:36:31 +0530

    UPDATE: Adding otpemail plugin

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,9 +1,6 @@
 import { createAuthClient } from "better-auth/react";
-
-if (!process.env.BETTER_AUTH_URL) {
-    throw new Error("Need better auth url - frontend url")
-}
+import { emailOTPClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL
-})
+  plugins: [emailOTPClient()],
+});
