8797859dd3003db9e1414bc68fac888b187f1a03
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-06-30 17:22:16 +0530

    UPDATE: Setting max amount limit

--- app/lib/validations/ProjectValidation.ts
+++ app/lib/validations/ProjectValidation.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const newProjectSchema = z.object({
     title: z.string().max(40).min(2, "Minimum 2 charactors"),
-    agreedcost: z.number(),
+    agreedcost: z.number().positive("Amount must be positive").max(1000000000, "Amount exceeds maximum limit"),
     deadline: z.date(),
     description: z.string().optional(),
     freelancerId: z.string(),
