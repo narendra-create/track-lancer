6520fce5d8fc0ed3ca1243aae2414b46c9d2e484
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-07-08 17:01:05 +0530

    FEAT: New utitlity to greet acording to time

--- app/lib/utilitys.ts
+++ app/lib/utilitys.ts
@@ -71,3 +71,27 @@ export function generateCode(length = 8) {
     }
     return code;
 }
+
+export const getGreeting = (): string => {
+    const hour = Number(
+        new Intl.DateTimeFormat("en-IN", {
+            hour: "numeric",
+            hour12: false,
+            timeZone: "Asia/Kolkata",
+        }).format(new Date())
+    );
+
+    if (hour >= 5 && hour < 12) {
+        return "Good Morning";
+    }
+
+    if (hour >= 12 && hour < 17) {
+        return "Good Afternoon";
+    }
+
+    if (hour >= 17 && hour < 21) {
+        return "Good Evening";
+    }
+
+    return "Good Night";
+};
