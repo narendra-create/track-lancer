8c2d25be6f6569902114f222dccb3a2393df4032
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-07-15 16:44:21 +0530

    UPDATE: Moving the file

--- app/api/activity/route.ts
+++ app/api/activity/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getActivitys } from "@/app/lib/controllers/activityController";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
     try {
         const { searchParams } = req.nextUrl;
